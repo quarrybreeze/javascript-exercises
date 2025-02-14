0a77ef0d2b6c543cbfe20d3987abea800c4bb1bb
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-14 14:07:12 -0800

    finished calculator assignment, on to the next!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let totalSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    totalSum = totalSum + arr[i];
+  }
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let totalProduct = 1;
+  for (let i = 0; i <arr.length; i++) {
+    totalProduct = totalProduct * arr[i];
+  }
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let totalProduct = 1;
+  for (let i = 0; i < b; i++) {
+    totalProduct = totalProduct * a;
+  }
+  return totalProduct;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let totalProduct = 1;
+  if (a == 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= a; i++) {
+      totalProduct = totalProduct * i;
+    }
+    return totalProduct;
+  }
 };
 
 // Do not edit below this line
