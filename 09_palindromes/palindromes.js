f6941a3fd73566d11abf889709b9c466e18d8a71
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-14 16:19:59 -0800

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,45 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringNoPunctuation = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    stringNoPunctuation = stringNoPunctuation.toLowerCase();
+    // console.log(stringNoPunctuation);
+    let arrRaw = stringNoPunctuation.split("")
+    // console.log(arrRaw);
 
+    let cleanedArr = arrRaw.filter(function(letter) {
+        return letter !== " ";
+    })
+    // console.log(cleanedArr);
+    let cleanedString = cleanedArr.join();
+
+    let reversedArr = cleanedArr.reverse();
+    // console.log(reversedArr);
+
+
+    let reversedString = reversedArr.join();
+
+    // console.log(cleanedString);
+    // console.log(reversedString);
+
+    if(cleanedString == reversedString) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+
+    // console.log(cleanedArr == reversedArr)
+
+    // if (cleanedArr == reversedArr) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 };
+
+
+
+// palindromes("I am, a string!");
 
 // Do not edit below this line
 module.exports = palindromes;
