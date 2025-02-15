f64b36c2615595ae792fcca8870df58c1ee101c3
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-14 17:23:29 -0800

    finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (int) {
+    let num = 0;
 
+    if (typeof int === "string") {
+        num = Number(int);
+    } else {
+        num = int;
+    }
+
+
+    let fibArr = [];
+    fibArr[0] = 0;
+    fibArr[1] = 1;
+
+    if (num >= 2) {
+        for (let i = 2; i <= num; i++) {
+            fibArr[i] = fibArr[(i-1)] + fibArr[(i-2)];
+            // console.log(fibArr[i]);
+        }
+    } else if ( num < 0) {
+        return "OOPS";
+    }
+
+    return fibArr[num];
 };
 
 // Do not edit below this line
