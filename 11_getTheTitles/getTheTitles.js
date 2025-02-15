7346e6a231df9a728596edfb944cf738cc509a73
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-14 18:01:39 -0800

    finished javascript exercises lesson

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titleArr = [];
 
+    for (i=0; i < arr.length; i++) {
+        titleArr[i] = arr[i].title;
+    }
+    return titleArr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+const currentYear = (new Date()).getFullYear();
+
+    const oldest = arr.sort(function(a,b) {
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = currentYear;
+        }
+        if (a.yearOfBirth == undefined) {
+            a.yearOfBirth = currentYear;
+        }
+
+        if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = currentYear;
+        }
+        if (b.yearOfBirth == undefined) {
+            b.yearOfBirth = currentYear;
+        }
+
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
